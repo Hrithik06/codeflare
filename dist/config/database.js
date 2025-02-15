fe366b2b92505be8efaade567a81798f6c2d86a1
Hrithik06 <el99el03@gmail.com>
2025-02-15 17:40:23 +0530

    login api, encrypt password using bccrypt

--- dist/config/database.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://codeflareapp:d18aYjZ3icxMmAav@cluster0codeflare.vp3x4.mongodb.net/");
-};
-export { connectDB };
