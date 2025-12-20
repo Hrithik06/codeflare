b746b81de686b062f4e293fb5694345e47f4dc0b
Hrithik06 <el99el03@gmail.com>
2025-12-20 23:16:15 +0530

    Remove unused Env variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,24 +8,24 @@ const nodeEnv = process.env.NODE_ENV || "development";
 console.log("Running in : ", nodeEnv);
 // Select env file based on NODE_ENV
 const envFile =
-  nodeEnv === "production" ? ".env.production" : ".env.development";
+	nodeEnv === "production" ? ".env.production" : ".env.development";
 dotenv.config({
-  path: path.resolve(envFile),
+	path: path.resolve(envFile),
 });
 
-// Zod check
+//Zod check
 const parsedEnv = envZodSchema.parse(process.env);
 
 // Export config
 export const config = {
-  DB_USERNAME: parsedEnv.DB_USERNAME,
-  DB_PASSWORD: parsedEnv.DB_PASSWORD,
-  DB_CLUSTER: parsedEnv.DB_CLUSTER,
-  ORIGIN: parsedEnv.ORIGIN,
-  PORT: parsedEnv.PORT,
-  JWT_SECRET_KEY: parsedEnv.JWT_SECRET_KEY,
-  BREVO_API_KEY: parsedEnv.BREVO_API_KEY,
-  SITE_ADMIN_EMAIL_FOR_BREVO: parsedEnv.SITE_ADMIN_EMAIL_FOR_BREVO,
-  NO_REPLY_BREVO: parsedEnv.NO_REPLY_BREVO,
-  CONTACT_FORM_NO_REPLY_BREVO: parsedEnv.CONTACT_FORM_NO_REPLY_BREVO,
+	DB_USERNAME: parsedEnv.DB_USERNAME,
+	DB_PASSWORD: parsedEnv.DB_PASSWORD,
+	DB_CLUSTER: parsedEnv.DB_CLUSTER,
+	ORIGIN: parsedEnv.ORIGIN,
+	PORT: parsedEnv.PORT,
+	JWT_SECRET_KEY: parsedEnv.JWT_SECRET_KEY,
+	BREVO_API_KEY: parsedEnv.BREVO_API_KEY,
+	SITE_ADMIN_EMAIL_FOR_BREVO: parsedEnv.SITE_ADMIN_EMAIL_FOR_BREVO,
+	NO_REPLY_BREVO: parsedEnv.NO_REPLY_BREVO,
+	CONTACT_FORM_NO_REPLY_BREVO: parsedEnv.CONTACT_FORM_NO_REPLY_BREVO,
 };
--- src/schemas/EnvFile.zod.ts
+++ src/schemas/EnvFile.zod.ts
@@ -1,19 +1,17 @@
 import { z } from "zod";
 
 export const envZodSchema = z.object({
-  DB_USERNAME: z.string(),
-  DB_PASSWORD: z.string(),
-  DB_CLUSTER: z.string(),
-  ORIGIN: z.string().url(),
-  JWT_SECRET_KEY: z.string(),
-  PORT: z
-    .string()
-    .transform((val) => parseInt(val, 10))
-    .refine((val) => !isNaN(val), { message: "PORT must be a number" }),
-  AWS_ACCESS_KEY_ID: z.string(),
-  AWS_SECRET_ACCESS_KEY: z.string(),
-  BREVO_API_KEY: z.string(),
-  SITE_ADMIN_EMAIL_FOR_BREVO: z.string().email(),
-  NO_REPLY_BREVO: z.string().email(),
-  CONTACT_FORM_NO_REPLY_BREVO: z.string().email(),
+	DB_USERNAME: z.string(),
+	DB_PASSWORD: z.string(),
+	DB_CLUSTER: z.string(),
+	ORIGIN: z.string().url(),
+	JWT_SECRET_KEY: z.string(),
+	PORT: z
+		.string()
+		.transform((val) => parseInt(val, 10))
+		.refine((val) => !isNaN(val), { message: "PORT must be a number" }),
+	BREVO_API_KEY: z.string(),
+	SITE_ADMIN_EMAIL_FOR_BREVO: z.string().email(),
+	NO_REPLY_BREVO: z.string().email(),
+	CONTACT_FORM_NO_REPLY_BREVO: z.string().email(),
 });
